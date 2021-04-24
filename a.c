7ae1f5d94c8db5074847e98d8f54f1e1c27cda96
Henrique Nazário <henrique.nazario.rocha@gmail.com>
2021-04-23 23:31:38 -0300

    alteração para um caraleio

--- a.c
+++ a.c
@@ -1,121 +1,128 @@
-#include <stdlib.h>
+
+// void cadastro_produtos(S_produto *produtos, int qtde)
+// {
+// 	char nome_novo_produto[D_D_QTDD_DE_ITENS_DO_NOME_DO_PRODUTO];
+// 	int i;
+// 	char opcao;
+		
+// 		printf("\n\tRegistro de mercadorias\n\n");
+//     	printf("\nNome: ");
+// 		scanf("%s", nome_novo_produto);
+// 		fflush(stdin);
+
+// 		for (i = 0; i < qtd; i++) //verificando se o código já está cadastrado
+// 		{
+// 			if(strcmp(nome_novo_produto, produtos[i].nome) == 0) 
+// 			{
+// 				printf("\nMercadoria já cadastrada\n");
+// 				//system("cls");
+// 				printf("\nDeseja altera a quantidade ou preço do produto?(s/n)\n");
+// 				fflush(stdin);
+// 				opcao = getchar();
+// 				system("cls");
+
+// 				if(opcao=='s' || opcao=='S')
+// 					//altera_produtos(S_produto *produtos, int qtd, char *nome_novo_produto, int index);
+// 					 printf("\nsim\n");
+// 				else if(opcao=='n' || opcao=='N')
+// 					printf("\nnao");
+// 				else 
+// 					printf("Selecao invalida\n");
+// 				break;
+// 			}
+// 		}	
+// 		printf("\nPressione enter para sair.");
+// 		getchar();
+// }
+
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
-/*  fwrite(void * buffer, size_ttamanho, size_t cont, FILE * arquivo), 
-	em que buffer contem o que se deseja escrever, tamanho indica
-	o tamanho em bytes de cada elemento do buffer e cont indica 
-	quantos elementos são lidos/escritos.
-*/
-
-
-#define D_TAM_DO_NOME_DO_PRODUTO 20
-#define D_ESTRELA_NOTA_FICAL "\n******************************\n"
-
+#define D_QTDD_DE_ITENS 8
+#define D_D_QTDD_DE_ITENS_DO_NOME_DO_PRODUTO 20
 
 typedef struct 
 {
-	char nome[D_TAM_DO_NOME_DO_PRODUTO];
-	unsigned int qtd;
-	float preco;
+  char nome[D_D_QTDD_DE_ITENS_DO_NOME_DO_PRODUTO];
+  unsigned int qtd;
+  float preco;
 
-}	S_produto;
+} S_produto;
 
-void cadastro_produtos(S_produto *produtos, int qtde)
-{
-	char nome_novo_produto[D_TAM_DO_NOME_DO_PRODUTO];
-	int i;
-	char opcao;
-		
-		printf("\n\tRegistro de mercadorias\n\n");
-    	printf("\nNome: ");
-		scanf("%s", nome_novo_produto);
-		fflush(stdin);
+typedef struct 
+{	
+	S_produto pilha [D_QTDD_DE_ITENS];
+	int topo;
+} S_pilha;
 
-		for (i = 0; i < qtde; i++) //verificando se o código já está cadastrado
-		{
-			if(strcmp(nome_novo_produto, produtos[i].nome) == 0) 
-			{
-				printf("\nMercadoria já cadastrada\n");
-				//system("cls");
-				printf("\nDeseja altera a quantidade ou preço do produto?(s/n)\n");
-				fflush(stdin);
-				opcao = getchar();
-				system("cls");
+void Imprime_Vetor(S_produto *prod){
 
-				if(opcao=='s' || opcao=='S')
-					//altera_produtos(S_produto *produtos, int qtde, char *nome_novo_produto, int index);
-					 printf("\nsim\n");
-				else if(opcao=='n' || opcao=='N')
-					printf("\nnao");
-				else 
-					printf("Selecao invalida\n");
-				break;
-			}
-		}	
-		printf("\nPressione enter para sair.");
-		getchar();
+    for(int i = 0; i< D_QTDD_DE_ITENS; i++)
+        printf("%-2d\t%-10s\t%-.2f\n", prod[i].qtd, prod[i].nome, prod[i].preco);
+   printf("\n");
 }
 
-void nota_fiscal(S_produto *produtos, int qtd)
+void pilha_push(S_produto *prod, int *topo, S_produto novo_prod)
 {
-	float total = 0;
-	printf("Itens comprados\nQte\tDescrição\tValor\tfinal");
-	printf(D_ESTRELA_NOTA_FICAL);
-
-	for (int i = 0; i < qtd; ++i)
-	{
-		printf("%-2d\t%-10s\t%-.2f\n", produtos[i].qtd, produtos[i].nome, produtos[i].preco);
-		total+=produtos[i].preco;
-	}
-	
-	printf(D_ESTRELA_NOTA_FICAL);
-	printf("Total\t%-7c%-.2f\n", ' ',total);
+  
+    if (*topo == D_QTDD_DE_ITENS -1 )
+       printf("pilha cheia! ");
+    else
+    {
+        *topo = *topo + 1;
+        prod[*topo] = novo_prod;
+    }
 }
 
-int contar(char *string)
+S_produto pilha_pop(S_produto *prod, int *topo)
 {
-	for (int i = 0; string;)
-		if (*(i + string)!= '\0')
-			i++;
-		else
-			return i;
+	S_produto aux;
+    if (*topo ==  -1 )
+       printf("pilha Vazia!\n");
+    else
+    {
+        printf("Retirando valor!: %s\n", prod[*topo].nome);
+        aux = prod[*topo];
+        memset(&prod[*topo], 0, sizeof(S_produto));
+        *topo = *topo - 1;
+    }
+    return aux;
 }
-void func()
-{
-		 // if ((fp=fopen ("nomes.txt","wb")) != NULL) 
-	 // {
-	 // 	for (int i = 0; i < 2; ++i)
-	 // 	{
-		//  	fprintf(fp, "%d,"	 ,prod[i].qtd);
-		//  	fwrite(prod[i].nome, sizeof(char), contar(prod[i].nome), fp);
-		//  	fprintf(fp, ",%.2f\n",prod[i].preco);
-		 	
-		// }
-		// fclose(fp);
-	 // }
-}
-int main(int argc,char *argv[]) 
-{
-	 //FILE *fp;
-	 S_produto prod[2] = {
-	 						{"blablabla", 5	, 5.15},
-	 						{"teste"	, 52, 6.85}
-	 					 };
 
-	 // printf("%d\n", contar(prod[0].nome));
-	 cadastro_produtos(prod, 2);
-	 // if ((fp=fopen ("nomes.txt","wb")) != NULL) 
-	 // {
-	 // 	for (int i = 0; i < 2; ++i)
-	 // 	{
-		//  	fprintf(fp, "%d,"	 ,prod[i].qtd);
-		//  	fwrite(prod[i].nome, sizeof(char), contar(prod[i].nome), fp);
-		//  	fprintf(fp, ",%.2f\n",prod[i].preco);
-		 	
-		// }
-		// fclose(fp);
-	 // }
+void inicializa_pilha(S_pilha *pilha)
+{
+	memset(pilha, 0, sizeof(S_pilha));
+	pilha->topo = -1;	
+}
+
+int main(int argc, char const *argv[])
+{
+   	S_pilha caixa; 
+   	inicializa_pilha(&caixa);
+ //   	memset(&caixa, 0, sizeof(S_pilha));
+	// caixa.topo = -1;    	
+   	// {
+   	// 	{
+	   //      {"blablabla", 5 , 5.15},
+	   //      {"teste"  , 52, 6.85},
+	   //      {"henrique naza", 99, 0.6}
+	   //  },
+	   //  2
+    // };
+
+    S_produto prod_novo = {"prod novo", 25 , 6.85};
+
+    Imprime_Vetor(caixa.pilha);
+    
+    pilha_push(caixa.pilha, &caixa.topo, prod_novo);
+    Imprime_Vetor(caixa.pilha);
+
+    S_produto prod_novo2 = pilha_pop(caixa.pilha, &caixa.topo);
+    pilha_push(caixa.pilha, &caixa.topo, prod_novo2);
+
+    Imprime_Vetor(caixa.pilha);
+    scanf(" ");
+
 	return 0;
 }
---
+++ aaa.c
@@ -0,0 +1,104 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*  fwrite(void * buffer, size_ttamanho, size_t cont, FILE * arquivo), 
+  em que buffer contem o que se deseja escrever, tamanho indica
+  o tamanho em bytes de cada elemento do buffer e cont indica 
+  quantos elementos são lidos/escritos.
+*/
+
+#define D_QTDD_DE_ITENS 10
+#define D_TAM_DO_NOME_DO_PRODUTO 20
+#define D_ESTRELA_NOTA_FICAL "\n******************************\n"
+
+typedef struct 
+{
+  char nome[D_TAM_DO_NOME_DO_PRODUTO];
+  unsigned int qtd;
+  float preco;
+} S_produto;
+
+int contar(char *string)
+{
+  for (int i = 0; string;)
+    if (*(i + string)!= '\0')
+      i++;
+    else
+      return i;
+}
+
+void nota_fiscal(S_produto *produtos, int qtd)
+{
+  float total = 0;
+  printf("Itens comprados\nQte\tDescricao\tValor\tfinal");
+  printf(D_ESTRELA_NOTA_FICAL);
+
+  for (int i = 0; i < qtd; ++i)
+  {
+    printf("%-2d\t%-10s\t%-.2f\n", produtos[i].qtd, produtos[i].nome, produtos[i].preco);
+    total+=produtos[i].preco;
+  }
+  
+  printf(D_ESTRELA_NOTA_FICAL);
+  printf("Total\t%-7c%-.2f\n", ' ',total);
+}
+
+void escrever_arq(S_produto *prod, int qtd)
+{
+  FILE *arq;
+  if ((arq=fopen ("nomes.txt","w")) != NULL) 
+   {
+    for (int i = 0; i < qtd; ++i)
+    {
+      fprintf(arq, "%u,"  ,prod[i].qtd);
+      fwrite(prod[i].nome, sizeof(char), contar(prod[i].nome), arq);
+      fprintf(arq, ",%.2f\n",prod[i].preco);
+    }
+    fclose(arq);
+   }
+}
+
+int ler_arq(S_produto *prod)
+{
+  int i;
+  FILE *arq;
+
+  if ((arq = fopen("nomes.txt","r")) != NULL) 
+   {
+    for(i=0; i < D_QTDD_DE_ITENS && (arq != NULL);)
+    {
+      fscanf(arq,"%u,%[^,]s",&prod[i].qtd, prod[i].nome);
+      fscanf(arq,",%f", &prod[i].preco); 
+     
+     if('\n'==fgetc(arq))
+        i++;
+     else
+      break;
+    }
+    fclose(arq);
+   }
+   return i;
+}
+
+int main()
+{
+  S_produto prod[3] = 
+  {
+      {"blablabla", 5 , 5.15},
+      {"teste"  , 52, 6.85},
+      {"henrique naza", 99, 0.6}
+  };
+  escrever_arq(prod, 3);  
+
+  S_produto prodiii[10]; 
+  memset(prodiii, 0, 10*sizeof(S_produto));
+
+  int num = ler_arq(prodiii);
+
+  printf("%d\n", num);
+  nota_fiscal(prodiii, num);
+  scanf(" ");
+
+ return 0;
+}
